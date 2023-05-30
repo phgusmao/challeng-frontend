6d03312828d031ba0e205b454d076f77b36faec3
phgusmao <pedro.gusmao1998@hotmail.com>
2023-05-30 12:14:16 -0300

    feat: first commit

---
+++ src/components/primary-input.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const PrimaryInput = styled.input`
+    width: 352px;
+    border-radius: 8px;
+    padding: 10px 16px;
+
+    background: var(--bg-secondary);
+
+    font-family: inherit;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+`
